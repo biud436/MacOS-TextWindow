706c22c9cbb48fa48f5734523cded4950e99a5c2
biud436 <biud436@gmail.com>
2022-04-04 21:47:29 +0900

    feat : add tagRECT

---
+++ SDL2/types.h
@@ -0,0 +1,19 @@
+//
+//  types.h
+//  SDL2
+//
+//  Created by 어진석 on 2022/04/04.
+//
+
+#ifndef types_h
+#define types_h
+
+typedef BYTE unsigned char;
+typedef struct tagRECT {
+    unsigned long left;
+    unsigned long top;
+    unsigned long right;
+    unsigned long bottom;
+} RECT, *PRECT, *NPRECT, *LPRECT;
+
+#endif /* types_h */
